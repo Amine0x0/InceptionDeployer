49e415ff18849f39cb96a52d25b1b8ef9100179f
Amine0x0 <abbadiamine6@gmail.com>
2026-07-24 18:27:54 +0100

    more structure, looks good for now, i still have to fill the impl

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -9,9 +9,14 @@ import (
 
 func CreateStructure(basePath string) {
 	directories := []string{
-		filepath.Join(basePath, "src", "requirements", "wordpress"),
-		filepath.Join(basePath, "src", "requirements", "mariadb"),
-		filepath.Join(basePath, "src", "requirements", "nginx"),
+		filepath.Join(basePath, "secrets"),
+		filepath.Join(basePath, "srcs"),
+		filepath.Join(basePath, "srcs", "requirements", "nginx", "conf"),
+		filepath.Join(basePath, "srcs", "requirements", "nginx", "tools"),
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", "conf"),
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", "tools"),
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", "conf"),
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", "tools"),
 	}
 
 	for _, dir := range directories {
@@ -21,7 +26,43 @@ func CreateStructure(basePath string) {
 			os.Exit(1)
 		}
 	}
-	log.Info("Project directory structure created successfully!")
+
+	files := []string{
+		filepath.Join(basePath, "Makefile"),
+
+		filepath.Join(basePath, "secrets", "credentials.txt"),
+		filepath.Join(basePath, "secrets", "db_password.txt"),
+		filepath.Join(basePath, "secrets", "db_root_password.txt"),
+
+		filepath.Join(basePath, "srcs", "docker-compose.yml"),
+		filepath.Join(basePath, "srcs", ".env"),
+
+		filepath.Join(basePath, "srcs", "requirements", "nginx", "Dockerfile"),
+		filepath.Join(basePath, "srcs", "requirements", "nginx", ".dockerignore"),
+		filepath.Join(basePath, "srcs", "requirements", "nginx", "conf", "nginx.conf"),
+		filepath.Join(basePath, "srcs", "requirements", "nginx", "tools", "entrypoint.sh"),
+
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", "Dockerfile"),
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", ".dockerignore"),
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", "conf", "www.conf"),
+		filepath.Join(basePath, "srcs", "requirements", "wordpress", "tools", "entrypoint.sh"),
+
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", "Dockerfile"),
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", ".dockerignore"),
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", "conf", "50-server.cnf"),
+		filepath.Join(basePath, "srcs", "requirements", "mariadb", "tools", "entrypoint.sh"),
+	}
+
+	for _, file := range files {
+		f, err := os.Create(file)
+		if err != nil {
+			log.Error("Failed to create file", "path", file, "err", err)
+			continue
+		}
+		f.Close()
+	}
+
+	log.Info("Mandatory Inception scaffolding created successfully!")
 }
 
 func GenerateConfigs(basePath string) {
---
+++ internal/wordpressgen/wordpressgen.go
@@ -0,0 +1,1 @@
+package wordpressgen
